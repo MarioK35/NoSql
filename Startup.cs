f7493c6344c442199232e672bb5d976104318e50
Mario <mariops3gow3@hotmail.com>
2020-02-11 15:05:09 -0600

    se agrego Swagger UI

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using BooksApi.Models;
 using Microsoft.Extensions.Options;
 using BooksApi.Services;
+using Microsoft.OpenApi.Models;
 
 namespace BooksApi
 {
@@ -38,16 +39,60 @@ namespace BooksApi
             services.AddSingleton<BookService>();
             
             services.AddControllers();
+
+
+             #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v0.1" });
+                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                    { 
+                    In = ParameterLocation.Header, 
+                    Description = "Please insert JWT with Bearer into field", 
+                    Name = "Authorization", Type = SecuritySchemeType.ApiKey 
+                    });
+                    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                        {
+                            {
+                                new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = "Bearer"
+                                    },
+                                    Scheme = "oauth2",
+                                    Name = "Bearer",
+                                    In = ParameterLocation.Header,
+                                },
+                                new List<string>()
+                            }
+                        });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            #endregion
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
